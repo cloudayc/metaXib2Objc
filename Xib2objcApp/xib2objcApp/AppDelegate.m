1c904a408e9b6c1be3683fd461fbb09b0319fdee
cloudayc <cloudayc@gmail.com>
2013-10-30 00:19:42 +0800

    add GUI

---
+++ Xib2objcApp/xib2objcApp/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  Xib2objcApp
+//
+//  Created by cloudayc on 10/30/13.
+//  Copyright (c) 2013 cloudayc. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ Xib2objcApp/xib2objcApp/xib2objcApp/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  xib2objcApp
+//
+//  Created by cloudayc on 10/30/13.
+//  Copyright (c) 2013 cloudayc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
