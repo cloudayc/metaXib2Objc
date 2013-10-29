09125ec5ffc9a3a694151ad34e3c997ce9ef8dec
cloudayc <cloudayc@gmail.com>
2013-10-30 00:09:12 +0800

    add GUI

---
+++ xib2objcApp/xib2objApp/xib2objApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  xib2objApp
+//
+//  Created by cloudayc on 10/30/13.
+//  Copyright (c) 2013 cloudayc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ xib2objcApp/xib2objApp/xib2objAppTests/xib2objAppTests.m
@@ -0,0 +1,34 @@
+//
+//  xib2objAppTests.m
+//  xib2objAppTests
+//
+//  Created by cloudayc on 10/30/13.
+//  Copyright (c) 2013 cloudayc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface xib2objAppTests : XCTestCase
+
+@end
+
+@implementation xib2objAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
